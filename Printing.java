7541a367ec8bf1f0ccc26f51839d2ce74c4faa2d
seikhchilli <saurabhkrsingh2001@gmail.com>
2022-01-13 19:53:46 +0530

    println and print

---
+++ Printing.java
@@ -0,0 +1,11 @@
+public class Printing {
+    public static void main(String[] args) {
+        int num = 100;
+        
+        System.out.println("Vlaue of num is: " + num); //Ends line with a new line character
+
+        num = num * 2;
+
+        System.out.print("Value of num * 2 is: " + num); //Does not end line with a new line chracter
+    }
+}
