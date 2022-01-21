f339c26f5cc49f7115a017f3f1a58e2d10de584d
seikhchilli <saurabhkrsingh2001@gmail.com>
2022-01-21 20:56:57 +0530

    parsing and typecasting

---
+++ Type.java
@@ -0,0 +1,13 @@
+public class Type {
+    public static void main(String args[]){
+        float f2 = (float)4.5;//explicit
+        byte b = (byte)130;//explicit
+        long l1 = 15;//implicit
+        String s1 = "12";
+        String s2 = "13";
+        int num2 = Integer.parseInt(s2);
+        int num1 = Integer.parseInt(s1);
+        System.out.println(num1 + num2);
+    }
+    
+}
